4470d51a1d2eeaa6c8f9a13088ded5023e39012e
Guilherme Campos <guicamppos12@gmail.com>
2026-04-10 16:52:58 -0300

    feat: ajustar estilo do footer para melhor responsividade

--- src/components/share/Footer.tsx
+++ src/components/share/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <footer className="bg-[#0a0a0a] border-t border-[rgba(212,175,55, 0.2)] py-8 ">
-            <div className="max-w-361 mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex flex-col md:flex-row items-center justify-between gap-6">
-                    <div className="text-[#a0a0a0] text-sm text-center md:text-left ">
+        <footer className=" bg-[#0a0a0a] border-t border-[rgba(212,175,55,0.2)] py-8 mx-4 sm:mx-6 lg:mx-8">
+            <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex flex-col items-center justify-center gap-6">
+                    <div className="text-[#a0a0a0] text-sm text-center md:text-left">
                         &copy; {new Date().getFullYear()} Barbearia Navalha. Todos os direitos reservados.
                     </div>
                 </div>
