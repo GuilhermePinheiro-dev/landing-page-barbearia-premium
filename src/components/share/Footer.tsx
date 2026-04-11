be576105e4512b07713bee376f55337ecbcfeb20
Guilherme Campos <guicamppos12@gmail.com>
2026-04-11 18:46:14 -0300

    fix: ajustando margin do footer

--- src/components/share/Footer.tsx
+++ src/components/share/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className=" bg-[#0a0a0a] border-t border-[rgba(212,175,55,0.2)] py-8 mx-4 sm:mx-6 lg:mx-8">
+        <footer className=" bg-[#0a0a0a] border-t border-[rgba(212,175,55,0.2)] py-8">
             <div className=" mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col items-center justify-center gap-6">
                     <div className="text-[#a0a0a0] text-sm text-center md:text-left">
